66b60e8795146dacf865739396ddc315eeef8728
Tanvir <tanvirislam29122000@gmail.com>
2025-11-11 03:18:54 +0600

    commit

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-import dotenv from "dotenv";
-dotenv.config();
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
 
 const app = express();
 const port = process.env.PORT || 5000;
